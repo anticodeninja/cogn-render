1c8fe6fa5515910ff1426c72aa0f8eefa8e6d592
zZLOiz <www.zloi@gmail.com>
2017-02-28 20:11:11 +0700

    Formulas for n-simplex transformation were simplified

--- utils/simplex-transformation.js
+++ utils/simplex-transformation.js
@@ -2,17 +2,20 @@ var SimplexTransformation = function(size) {
     this.size = size;
 }
 
-SimplexTransformation.prototype.legKoef = 1 / Math.cos(Math.PI/6);
+var pLength = {
+    3: Math.sqrt(3) / 2,
+    4: Math.sqrt(2 / 3)
+}
 
 var pAngles = {
     3: [
-        -Math.PI / 3,
-        Math.PI
+        Math.PI / 3,
+        0
     ],
     4: [
-        [Math.PI - Math.acos(Math.sqrt(2/3)), 0],
-        [Math.PI / 2,  Math.PI / 6 * 5],
-        [Math.PI / 2, -Math.PI / 2]
+        [Math.PI / 3, -Math.asin(1 / 3)],
+        [Math.PI / 3, -Math.PI / 2],
+        [0, 0]
     ]
 }
 
@@ -46,51 +49,52 @@ var mKoef = {
     // precalc 2-simplex
     for (i = 0; i < 2; ++i) {
 	pKoef[3].push([
-	    Math.cos(pAngles[3][i]),
-	    Math.sin(pAngles[3][i])
+	    Math.cos(pAngles[3][i]) / pLength[3],
+	    Math.sin(pAngles[3][i]) / pLength[3]
 	]);
     }
     for (i = 0; i < 3; ++i) {
 	mKoef[3].push([
-	    Math.cos(mAngles[3][i]),
-	    Math.sin(mAngles[3][i])
+	    Math.cos(mAngles[3][i]) / pLength[3],
+	    Math.sin(mAngles[3][i]) / pLength[3]
 	]);
     }
 
     // precalc 3-simplex
     for (i = 0; i < 3; ++i) {
-	mKoef[4].push([
-	    Math.sin(mAngles[4][i][0]) * Math.cos(mAngles[4][i][1]),
-	    Math.cos(mAngles[4][i][0]),
-	    Math.sin(mAngles[4][i][0]) * Math.sin(mAngles[4][i][1])
+	pKoef[4].push([
+            Math.cos(pAngles[4][i][0]) / pLength[4],
+	    Math.sin(pAngles[4][i][0]) * Math.cos(pAngles[4][i][1]) / pLength[4],
+	    Math.sin(pAngles[4][i][0]) * Math.sin(pAngles[4][i][1]) / pLength[4]
 	]);
     }
     for (i = 0; i < 4; ++i) {
 	mKoef[4].push([
-	    Math.sin(mAngles[4][i][0]) * Math.cos(mAngles[4][i][1]),
-	    Math.cos(mAngles[4][i][0]),
-	    Math.sin(mAngles[4][i][0]) * Math.sin(mAngles[4][i][1])
+            Math.cos(mAngles[4][i][0]) / pLength[4],
+	    Math.sin(mAngles[4][i][0]) * Math.cos(mAngles[4][i][1]) / pLength[4],
+	    Math.sin(mAngles[4][i][0]) * Math.sin(mAngles[4][i][1]) / pLength[4]
 	]);
     }
 })();
 
 SimplexTransformation.prototype.toPoint = function(h, path) {
     var i, j,
-        chunkLength,
         h = this.normalize(h),
-        height = this.size,
         koef = pKoef[h.length],
         coord = new Float32Array(h.length - 1),
         chunk;
 
-    coord[1] = this.size / 3 * 2;
-
+    // centerize start point
     for (i = 0; i < h.length - 1; ++i) {
-        height -= h[i];
-        chunkLength = height * this.legKoef;
-
         for (j = 0; j < h.length - 1; ++j) {
-            coord[j] += chunkLength * koef[i][j];
+            coord[j] += (-this.size / h.length) * koef[i][j] ;
+        }
+    }
+
+    // move to necessary
+    for (i = 0; i < h.length - 1; ++i) {
+        for (j = 0; j < h.length - 1; ++j) {
+            coord[j] += h[i] * koef[i][j];
         }
 
         if (path)
